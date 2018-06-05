4eb1e1f63eea5956f862b0a577025b2c89de9da7
DFloor Danny <37215615+dan-homoncik@users.noreply.github.com>
2018-06-05 15:27:51 -0400

    finished psychic game

---
+++ assets/javascript/game.js
@@ -0,0 +1,84 @@
+// make an array for the letters available for the computer to guess from
+
+var computerChoices = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+// establish user starting variables
+
+var wins = 0;
+var losses = 0;
+var guessesLeft = 10;
+var guessedLetters = []; //make javascript create an array for each letter guessed
+var computerGuess = null;
+
+
+
+
+// get the computer to generate a new random letter every time it resets
+
+var newComputerGuess = function() { 
+    this.computerGuess = this.computerChoices[Math.floor(Math.random() * this.computerChoices.length)];
+};
+
+// setting a variable for the html element for guesses left and guesses so far. 
+// used on new game to reset the html element
+
+var guessCounter = function () {
+    document.querySelector("#guessesRemaining").innerHTML = "Guesses Left: " + guessesLeft;
+};
+
+var guessesSoFar = function () {
+    document.querySelector("#guessesSoFar").innerHTML = "You guesses so far: " + guessedLetters.join(", ");
+};
+
+// this function will reset all of the variables and restart the game
+
+var gameReset = function() {
+guesses = 10;
+guessesLeft = 10;
+guessedLetters = [];
+
+newComputerGuess();
+guessCounter();
+guessesSoFar();
+
+};
+
+// STARTS THE GAME
+guessCounter();
+newComputerGuess();
+
+// establishing what key the user has pushed, then determining what to do next
+
+document.onkeyup = function(event) {
+
+    // get the user's guess
+    var userGuess = event.key.toLowerCase();
+
+    // check for the win, reset the game
+    if (userGuess === computerGuess) {  
+        wins++;
+        document.querySelector("#wins").innerHTML = "Wins: " + wins;
+        alert("You are a psychic! Do it again!");
+        gameReset();
+    };
+
+    // evaluates whether the two guesses are the same 
+    // ADDITIONAL NOTE: I attempted to evaluate for duplicates, but the loop wasn't working as intended
+    if (userGuess !== computerGuess) {
+        guessedLetters.push(userGuess);
+        guessesSoFar();
+        guessesLeft--;
+        guessCounter();
+    };
+
+    // check for the loss, reset the game
+    if (guessesLeft === 0) {
+        losses++;
+        document.querySelector("#losses").innerHTML = "Losses: " + losses;
+        alert("You are no psychic! But try again if I'm wrong!");
+        gameReset();
+    };
+
+
+};
+
